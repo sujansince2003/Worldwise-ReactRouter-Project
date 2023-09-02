e8d7c4007d19bdcaab92f25f45713a81849758cc
sujan khatri <sujansince2059@gmail.com>
2023-09-02 22:53:13 +0545

    <Navigate/>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
@@ -47,7 +47,7 @@ useEffect(function(){
    <Route  path="pricing" element={<Pricing/>} />
    <Route  path="login" element={<Login />} />
    <Route  path="app" element={<AppLayout />}>
-     <Route index element={<CityList  cities={cities} isLoading={isLoading} />} /> 
+     <Route index element={<Navigate replace to="cities " />} /> 
      <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
      <Route path="cities/:id" element ={<City />} />
      <Route path="countries" element={<CountriesList cities={cities} isLoading={isLoading} />} />
