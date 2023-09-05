41c5b8924a625b912401d9016f86d913beb2a982
sujan khatri <sujansince2059@gmail.com>
2023-09-05 15:53:12 +0545

    consuming contexT

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
@@ -8,38 +10,24 @@ import CityList from "./components/CityList";
 import CountriesList from "./components/CountriesList"
 import City from "./components/City";
 import Form from "./components/Form"
+
+
 import { useEffect, useState } from "react";
+import { CitiesProvider } from "./Contexts/CitiesContext";
+
+
 const App = () => {
 
-  //fetching data
-const fetchlink="http://localhost:8000"
 
-  const [cities,setCities]=useState([]);
-  const [isLoading,setIsloading]=useState(false)
 
-useEffect(function(){
-  async function fetchcities()
-  {
-    try{
-      setIsloading(true)
-         const res=await fetch(`${fetchlink}/cities`)
-         const data=await res.json();
-         setCities(data);
-        }
-    catch(err)
-    {
-      console.log(err.message);
-    }
-    finally{
-      setIsloading(false)
-    }
-  }
-  fetchcities()
-},[])
+
+ 
 
 
 
   return ( 
+    <CitiesProvider>
+
     <BrowserRouter>
     <Routes>
    <Route  path="/" element={<Homepage/>} />
@@ -48,15 +36,16 @@ useEffect(function(){
    <Route  path="login" element={<Login />} />
    <Route  path="app" element={<AppLayout />}>
      <Route index element={<Navigate replace to="cities " />} /> 
-     <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+     <Route path="cities" element={<CityList  />} />
      <Route path="cities/:id" element ={<City />} />
-     <Route path="countries" element={<CountriesList cities={cities} isLoading={isLoading} />} />
+     <Route path="countries" element={<CountriesList  />} />
      <Route path="form" element={<Form />} />
    </Route> 
    <Route path="*" element={<div>Error</div>} />
 
     </Routes>
     </BrowserRouter>
+    </CitiesProvider>
    );
 }
  
---
+++ src/Contexts/CitiesContext.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable react/prop-types */
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+const fetchlink="http://localhost:8000"
+
+const citiesContext=createContext()
+function CitiesProvider({children})
+{
+
+    
+    const [cities,setCities]=useState([]);
+    const [isLoading,setIsloading]=useState(false)
+  
+  useEffect(function(){
+    async function fetchcities()
+    {
+      try{
+        setIsloading(true)
+           const res=await fetch(`${fetchlink}/cities`)
+           const data=await res.json();
+           setCities(data);
+          }
+      catch(err)
+      {
+        console.log(err.message);
+      }
+      finally{
+        setIsloading(false)
+      }
+    }
+    fetchcities()
+  },[])
+
+  return (<citiesContext.Provider value={{
+    cities,
+    isLoading
+  }}>
+{children}
+  </citiesContext.Provider>)
+
+}
+
+
+//creating custom hook to consume context
+
+function useCities()
+{
+    const context=useContext(citiesContext)
+    if(context===undefined)
+    throw new Error(" context is used outside the cityProvider function")
+    return context;
+}
+
+
+export {CitiesProvider}
+
