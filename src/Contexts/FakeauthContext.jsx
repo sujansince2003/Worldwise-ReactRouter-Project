a6c19431dce9bfba1fc63a35a967669c6b873990
sujan khatri <sujansince2059@gmail.com>
2023-09-09 20:02:51 +0545

    challage 1

--- src/Contexts/FakeauthContext.jsx
+++ src/Contexts/FakeauthContext.jsx
@@ -6,16 +6,49 @@ const initialstate = {
   isAuthenticated: false,
 };
 
+function render(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      console.log("default");
+  }
+}
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
 // eslint-disable-next-line react/prop-types
 function AuthProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialstate);
   const { user, isAuthenticated: IA } = state;
 
-  function login(email, pass) {}
+  function login(email, pass) {
+    if (email === FAKE_USER.email && pass === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
 
-  function logout() {}
+  function logout() {
+    dispatch({ type: "logout" });
+  }
 
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        IA,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
